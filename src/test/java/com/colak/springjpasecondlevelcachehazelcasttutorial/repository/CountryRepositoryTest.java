0613a0d40e5bb2ce4d01b57beb0348f53aedcfdb
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-07 15:18:04 +0300

    test for l2 hit

--- src/test/java/com/colak/springjpasecondlevelcachehazelcasttutorial/repository/CountryRepositoryTest.java
+++ src/test/java/com/colak/springjpasecondlevelcachehazelcasttutorial/repository/CountryRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,7 +21,10 @@ class CountryRepositoryTest {
         List<Country> all = repository.findAll();
         assertThat(all).isNotEmpty();
 
-        all = repository.findAll();
-        assertThat(all).isNotEmpty();
+        // Fo L2 hit we need to find by id
+        Optional<Country> byId = repository.findById(1L);
+        assertThat(byId).isPresent();
+        Country country = byId.get();
+        assertThat(country).isNotNull();
     }
 }
