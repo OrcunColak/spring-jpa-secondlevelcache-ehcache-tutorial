eb37d5c4032a0fe34579c36067cf74d7ef14641d
Orçun Çolak <orcun.colak@hazelcast.com>
2024-04-03 09:44:38 +0300

    show caching providers

--- src/main/java/com/colak/springjpasecondlevelcachehazelcasttutorial/SpringJpaSecondLevelCacheTutorialApplication.java
+++ src/main/java/com/colak/springjpasecondlevelcachehazelcasttutorial/SpringJpaSecondLevelCacheTutorialApplication.java
@@ -1,13 +1,27 @@
 package com.colak.springjpasecondlevelcachehazelcasttutorial;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+
 @SpringBootApplication
-public class SpringJpaSecondLevelCacheTutorialApplication {
+@Slf4j
+public class SpringJpaSecondLevelCacheTutorialApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringJpaSecondLevelCacheTutorialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaSecondLevelCacheTutorialApplication.class, args);
+    }
 
+    // See https://stackoverflow.com/questions/51886101/spring-boot2-x-and-ehcache3-5-are-error-based-on-annotations/72642142#72642142
+    @Override
+    public void run(String... args) {
+        Iterable<CachingProvider> iterable = Caching.getCachingProviders();
+        iterable.forEach(cachingProvider -> {
+            log.info("cachingProvider=" + cachingProvider);
+        });
+    }
 }
